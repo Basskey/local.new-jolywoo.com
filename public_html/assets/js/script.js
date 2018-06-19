dcc6ded9ef8789b4e0f56bad777c7c55c37bb655
Basskey <basskey@bk.ru>
2018-06-19 06:15:45 +0300

    well done!

--- public_html/assets/js/script.js
+++ public_html/assets/js/script.js
@@ -1,4 +1,4 @@
-//gallery animation engine
+//gallery animation
 
 var idNum //
 function imgViewerOn(n) {
@@ -12,10 +12,22 @@ function imgViewerOff() {
 	$('.img-viewer').fadeOut(500);
 };
 
-//map animation engine
+//map animation
 
 $(document).ready(function() {
 	$('.map').click(function() {
 		$('.front-ground').fadeOut(1000);
 	});
+});
+
+//scrolling animation
+
+$(document).ready(function(){
+    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
+	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+        }
+	    return false; // выключаем стандартное действие
+    });
 });
