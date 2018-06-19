d4f8f6b921473e1060e189ac6597da95e189a6f9
Basskey <basskey@bk.ru>
2018-06-19 05:45:08 +0300

    general animation complete

--- public_html/assets/js/script.js
+++ public_html/assets/js/script.js
@@ -1,12 +1,21 @@
-//gallery engine
+//gallery animation engine
+
 var idNum //
 function imgViewerOn(n) {
 	idNum = n;
-	$('.img-viewer').css('display','flex');
-	$("#gal_" + idNum).css('display','block');
+	$('.img-viewer').fadeIn(500).css('display','flex');
+	$('#gal_' + idNum).css('display','block');
 };
 
 function imgViewerOff() {
-	$("#gal_" + idNum).css('display','none');
-	$('.img-viewer').css('display','none');
+	$('#gal_' + idNum).fadeOut(500);
+	$('.img-viewer').fadeOut(500);
 };
+
+//map animation engine
+
+$(document).ready(function() {
+	$('.map').click(function() {
+		$('.front-ground').fadeOut(1000);
+	});
+});
