7bece1bc263af89229928432abe3607facc9eff9
Basskey <basskey@bk.ru>
2018-06-19 07:00:53 +0300

    optimiZation correction

--- public_html/assets/js/script.js
+++ public_html/assets/js/script.js
@@ -12,6 +12,22 @@ function imgViewerOff() {
 	$('.img-viewer').fadeOut(500);
 };
 
+
+
+//slider animation
+
+$(document).ready(function(){
+	$('.gallery-slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		variableWidth: true,
+	});
+});
+
+
+
 //map animation
 
 $(document).ready(function() {
@@ -20,14 +36,16 @@ $(document).ready(function() {
 	});
 });
 
+
+
 //scrolling animation
 
 $(document).ready(function(){
-    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
+	$('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
 	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
-        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
-	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
-        }
-	    return false; // выключаем стандартное действие
-    });
+		if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+			$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+		}
+		return false; // выключаем стандартное действие
+	});
 });
