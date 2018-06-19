797aad5f94960e07481fcb8904d6b1fce09a9ad2
Basskey <basskey@bk.ru>
2018-06-19 07:18:11 +0300

    meh

--- public_html/assets/js/script.js
+++ public_html/assets/js/script.js
@@ -1,5 +1,4 @@
 //gallery animation
-
 var idNum //
 function imgViewerOn(n) {
 	idNum = n;
@@ -12,10 +11,7 @@ function imgViewerOff() {
 	$('.img-viewer').fadeOut(500);
 };
 
-
-
 //slider animation
-
 $(document).ready(function(){
 	$('.gallery-slider').slick({
 		slidesToShow: 3,
@@ -26,26 +22,20 @@ $(document).ready(function(){
 	});
 });
 
-
-
 //map animation
-
 $(document).ready(function() {
 	$('.map').click(function() {
 		$('.front-ground').fadeOut(1000);
 	});
 });
 
-
-
 //scrolling animation
-
 $(document).ready(function(){
-	$('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
-	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
-		if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
-			$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+	$('.go_to').click( function(){ //ловим клик по ссылке с классом go_to
+	var scroll_el = $(this).attr('href'); //возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+		if ($(scroll_el).length != 0) { //проверим существование элемента чтобы избежать ошибки
+			$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); //анимируем скроллинг к элементу scroll_el
 		}
-		return false; // выключаем стандартное действие
+		return false; //выключаем стандартное действие
 	});
 });
